5b618edfebe54edb92437e4f009abb46340c2c33
Marc <marcplanella27@gmail.com>
2025-07-13 13:44:30 +0200

    Actualització 31 despres aturada

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if (!MONGO_URI) {
 }
 
 // Conexión a MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(MONGO_URI)
     .then(() => console.log('Conectado a MongoDB Atlas'))
     .catch(err => {
         console.error('Error al conectar a MongoDB:', err);
